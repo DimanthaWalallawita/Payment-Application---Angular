226c50bd070d2107aff44e98e4172d080202ff0e
Dimantha Thihara Walallawita <87009586+DimanthaWalallawita@users.noreply.github.com>
2024-12-23 01:33:02 +0530

    UI changes

--- src/app/payment-details/payment-details.component.ts
+++ src/app/payment-details/payment-details.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { PaymentDetailFormComponent } from './payment-detail-form/payment-detail-form.component';
 import { PaymentDetailService } from '../shared/payment-detail.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-payment-details',
   standalone: true,
-  imports: [PaymentDetailFormComponent],
+  imports: [PaymentDetailFormComponent, CommonModule],
   templateUrl: './payment-details.component.html',
   styles: ``
 })
