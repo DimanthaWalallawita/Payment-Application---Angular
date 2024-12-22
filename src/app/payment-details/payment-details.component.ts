acd0e9d24974a334768d981a8f8851eb88e3849e
Dimantha Thihara Walallawita <87009586+DimanthaWalallawita@users.noreply.github.com>
2024-12-22 21:24:53 +0530

    First Commit

---
+++ src/app/payment-details/payment-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { PaymentDetailFormComponent } from './payment-detail-form/payment-detail-form.component';
+
+@Component({
+  selector: 'app-payment-details',
+  standalone: true,
+  imports: [PaymentDetailFormComponent],
+  templateUrl: './payment-details.component.html',
+  styles: ``
+})
+export class PaymentDetailsComponent {
+
+}
